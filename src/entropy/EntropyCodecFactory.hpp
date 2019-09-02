ed8516f40c1ac0cb8583ed48afd478a23e181c37
whattheblock <40543229@gm.nfu.edu.tw>
2019-09-03 03:27:00 +0800

    Improve handling on command line parameters

--- src/entropy/EntropyCodecFactory.hpp
+++ src/entropy/EntropyCodecFactory.hpp
@@ -93,8 +93,9 @@ namespace kanzi {
            return new NullEntropyDecoder(ibs);
 
        default:
-           string msg = "Unknown entropy codec type: ";
+           string msg = "Unknown entropy codec type: '";
            msg += char(entropyType);
+		   msg += '\'';
            throw invalid_argument(msg);
        }
    }
@@ -130,8 +131,9 @@ namespace kanzi {
            return new NullEntropyEncoder(obs);
 
        default:
-           string msg = "Unknown entropy codec type: ";
+           string msg = "Unknown entropy codec type: '";
            msg += char(entropyType);
+		   msg += '\'';
            throw invalid_argument(msg);
        }
    }
@@ -167,8 +169,9 @@ namespace kanzi {
            return "NONE";
 
        default:
-           string msg = "Unknown entropy codec type: ";
+           string msg = "Unknown entropy codec type: '";
            msg += char(entropyType);
+		   msg += '\'';
            throw invalid_argument(msg);
        }
    }
@@ -205,7 +208,7 @@ namespace kanzi {
        if (name == "NONE")
            return NONE_TYPE;
 
-       throw invalid_argument("Unsupported entropy codec type: " + name);
+	   throw invalid_argument("Unsupported entropy codec type: '" + name + "'");
    }
 }
 #endif
