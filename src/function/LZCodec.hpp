5e063ad4e686afdc6f6954b7efea0a1be05f63dc
WhatTheBlock <40543229@gm.nfu.edu.tw>
2021-09-28 16:39:06 +0800

    Rewrite the LZ codec for better compression ratio at nearly same speed.

--- src/function/LZCodec.hpp
+++ src/function/LZCodec.hpp
@@ -1,5 +1,5 @@
 /*
-Copyright 2011-2020 Frederic Langlet
+Copyright 2011-2017 Frederic Langlet
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 you may obtain a copy of the License at
@@ -16,22 +16,23 @@ limitations under the License.
 #ifndef _LZCodec_
 #define _LZCodec_
 
-#include <cstring>
+#include <cstring> // for memcpy
 #include "../Context.hpp"
 #include "../Function.hpp"
+#include "../Memory.hpp"
 
 namespace kanzi 
 {
 
    // Simple byte oriented LZ77 implementation.
-   // It is just LZ4 modified to use a bigger hash map.
-
+   // It is a modified LZ4 with a bigger window, a bigger hash map, 3+n*8 bit 
+   // literal lengths and 17 or 24 bit match lengths.
    class LZCodec : public Function<byte>
    {
    public:
-       LZCodec() { _buffer = new int[0]; _bufferSize = 0; }
-       LZCodec(Context&) { _buffer = new int[0]; _bufferSize = 0; }
-       ~LZCodec() { delete[] _buffer; _bufferSize = 0; }
+       LZCodec() { _hashes = new int[0]; _bufferSize = 0; }
+       LZCodec(Context&) { _hashes = new int[0]; _bufferSize = 0; }
+       ~LZCodec() { delete[] _hashes; _bufferSize = 0; }
 
        bool forward(SliceArray<byte>& src, SliceArray<byte>& dst, int length) THROW;
 
@@ -45,38 +46,32 @@ namespace kanzi
 
    private:
       static const uint LZ_HASH_SEED      = 0x7FEB352D;
-      static const uint HASH_LOG_SMALL    = 12;
-      static const uint HASH_LOG_BIG      = 16;
-      static const int MAX_DISTANCE       = (1 << 16) - 1;
-      static const int SKIP_STRENGTH      = 6;
-      static const int LAST_LITERALS      = 5;
+      static const uint HASH_LOG          = 18;
+      static const uint HASH_SHIFT        = 32 - HASH_LOG;
+      static const int MAX_DISTANCE1      = (1 << 17) - 1;
+      static const int MAX_DISTANCE2      = (1 << 24) - 1;
       static const int MIN_MATCH          = 4;
-      static const int MF_LIMIT           = 12;
-      static const int ML_BITS            = 4;
-      static const int ML_MASK            = (1 << ML_BITS) - 1;
-      static const int RUN_BITS           = 8 - ML_BITS;
-      static const int RUN_MASK           = (1 << RUN_BITS) - 1;
-      static const int COPY_LENGTH        = 8;
-      static const int MIN_LENGTH         = 14;
-      static const int MAX_LENGTH         = (32*1024*1024) - 4 - MIN_MATCH;
-      static const int SEARCH_MATCH_NB    = 1 << 6;
+      static const int MIN_LENGTH         = 16;
 
-      int* _buffer;
+      int32* _hashes;
       int _bufferSize;
 
       static int emitLength(byte block[], int len);
 
-      static int emitLastLiterals(byte src[], byte dst[], int runLength);
+      static int emitLastLiterals(const byte src[], byte dst[], int runLength);
 
       static bool sameInts(byte block[], int srcIdx, int dstIdx);
 
-      static void customArrayCopy(byte src[], byte dst[], int len);
+      static void emitLiterals(const byte src[], byte dst[], int len);
+
+      static int32 hash(const byte* p);
    };
 
-   inline void LZCodec::customArrayCopy(byte src[], byte dst[], int len)
+
+   inline void LZCodec::emitLiterals(const byte src[], byte dst[], int len)
    {
-	   for (int i = 0; i < len; i += 8)
-		   memcpy(&dst[i], &src[i], 8);
+       for (int i = 0; i < len; i += 8)
+           memcpy(&dst[i], &src[i], 8);
    }
 
    inline bool LZCodec::sameInts(byte block[], int srcIdx, int dstIdx)
@@ -84,18 +79,23 @@ namespace kanzi
        return *(reinterpret_cast<int32*>(&block[srcIdx])) == *(reinterpret_cast<int32*>(&block[dstIdx]));
    }
 
+   inline int32 LZCodec::hash(const byte* p)
+   {
+       return (LittleEndian::readInt32(p) * LZ_HASH_SEED) >> HASH_SHIFT;
+   }
+
    inline int LZCodec::emitLength(byte block[], int length)
    {
-	   int idx = 0;
+       int idx = 0;
 
        while (length >= 0xFF) {
-		   block[idx] = byte(0xFF);
-		   idx++;
-		   length -= 0xFF;
-	   }
+           block[idx] = byte(0xFF);
+           idx++;
+           length -= 0xFF;
+       }
 
-	   block[idx] = byte(length);
-	   return idx + 1;
+       block[idx] = byte(length);
+       return idx + 1;
    }
 }
 #endif
